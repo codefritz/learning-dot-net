3261a33381070603f947fa6095971485a200e4cb
André Charton <andre.charton@adevinta.com>
2026-01-05 15:14:09 +0100

    feat: add place holder for pascal

---
+++ Pascal.cs
@@ -0,0 +1,25 @@
+
+using System.Runtime.CompilerServices;
+
+namespace LearningDotNet
+{
+    class Demo
+    {
+        public static void RunPascal(int deep)
+        {
+            deep = deep > 0 ? deep : 3;
+            
+            Console.WriteLine($"Create Pasales triangle with deep : {deep}");
+            
+            string binaryString = "1001011001";
+
+            List<int> digits = binaryString.Select(c => c - '0').ToList();
+
+            Console.WriteLine($"Input: {binaryString}");
+            Console.WriteLine($"Outut: [{string.Join(" ", digits)}]");
+
+        }
+    }
+
+
+}
